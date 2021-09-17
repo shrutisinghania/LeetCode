662d9cc391c73e3d06a63da9c4abb91849670010
Shruti Singhania <shrutisinghania75@gmail.com>
2021-09-17 19:40:29 +0530

    Time: 5 ms (67.07%), Space: 41.3 MB (27.88%) - LeetHub

--- roman-to-integer/roman-to-integer.java
+++ roman-to-integer/roman-to-integer.java
@@ -1,77 +1,31 @@
 class Solution {
     public int romanToInt(String s) {
-        int sum = 0;
-        for(int i=0; i<s.length();i++){  
-            char c = s.charAt(i);  
-            switch(c){
+        int sum = 0, n = 0;
+        for(int i=s.length()-1; i>=0;i--){  
+            switch(s.charAt(i)){
                 case 'I':
-                    try{
-                        int n = s.charAt(i+1);
-                        if(n == 'V'){
-                            sum = sum+4;
-                            i = i+1;
-                            break;
-                        }
-                        if(n == 'X'){
-                            sum = sum+9;
-                            i = i+1;
-                            break;
-                        }
-                    }
-                    catch(StringIndexOutOfBoundsException e){                
-                    }
-                    sum = sum+1;
+                    n = 1;
                     break;
                 case 'V':
-                    sum = sum+5;
+                    n = 5;
                     break;
                 case 'X':
-                    try{
-                        int o = s.charAt(i+1);
-                        if(o == 'L'){
-                            sum = sum+40;
-                            i = i+1;
-                            break;
-                        }
-                        if(o == 'C'){
-                            sum = sum+90;
-                            i = i+1;
-                            break;
-                        }
-                    }
-                    catch(StringIndexOutOfBoundsException e){                
-                    }
-                    sum = sum+10;
+                    n = 10;
                     break;
                 case 'L':
-                    sum = sum+50;
+                    n = 50;
                     break;
                 case 'C':
-                    try{
-                        int h = s.charAt(i+1);
-                        if(h == 'D'){
-                            sum = sum+400;
-                            i = i+1;
-                            break;
-                        }
-                        if(h == 'M'){
-                            sum = sum+900;
-                            i = i+1;
-                            break;
-                        }
-                    }
-                    catch(StringIndexOutOfBoundsException e){                
-                    }
-                    sum = sum+100;
+                    n = 100;
                     break;
                 case 'D':
-                    sum = sum+500;
+                    n = 500;
                     break;
                 case 'M':
-                    sum = sum+1000;
+                    n = 1000;
                     break;
-                
             }
+            sum = (4*n<sum) ? sum-n : sum+n;
         }  
         return sum;
     }
