abdb9238a95da0d7a67e42df9006c840c834a484
Shruti Singhania <shrutisinghania75@gmail.com>
2021-09-23 15:32:04 +0530

    Time: 6 ms (99.98%), Space: 38 MB (93.94%) - LeetHub

--- palindrome-number/palindrome-number.java
+++ palindrome-number/palindrome-number.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
         int rem = 0, copy, rev = 0;
         copy = x;
         while (x!=0){
             rem = x % 10;
-            rem = (rem < 0) ? -rem : rem;
             rev = rev * 10 + rem;
             x = x / 10;
         }
