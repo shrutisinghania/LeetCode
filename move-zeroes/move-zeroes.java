533653739b8931162201545a721744ab3943843a
Shruti Singhania <shrutisinghania75@gmail.com>
2021-10-08 10:55:15 +0530

    Time: 26 ms (5.01%), Space: 40.1 MB (48.35%) - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,22 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int swap = 0;
         for(int i =0; i < nums.length; i++)
         {
             if (nums[i] == 0 &&  i+1 < nums.length)
             {
-                int j = i + 1, temp;
+                int j = i + 1;
                 while (nums[j] == 0 )
                 {
                     j++;
                     if (j == nums.length)
                         return;
                 }
-                temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                swap++;
+                exchange(nums, i, j);
             }
         }
+    }
+    
+    private void exchange(int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
