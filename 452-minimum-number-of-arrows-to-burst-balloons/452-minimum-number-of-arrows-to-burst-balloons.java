426ab9eead6dfacd32ffb002e3643c3a7ad2be4b
Shruti Singhania <shrutisinghania75@gmail.com>
2022-01-13 22:33:24 +0530

    Time: 136 ms (7.93%), Space: 116.6 MB (18.10%) - LeetHub

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        if (points == null || points.length == 0) return 0;
+        int count = 0;      // results
+        // minEnd : Key parameter "active set" for overlapping intervals, 
+        // e.g. the minimum ending point among all overlapping intervals;
+        int minEnd = Integer.MAX_VALUE;     
+        Arrays.sort(points,(a,b)->{
+            if(a[1]>b[1]) return 1;
+            else return -1;
+        });   // Sorting the intervals/pairs in ascending order of its starting point
+        
+        for (int[] in : points) {
+            // If the changing some states, record some information, and start a new active set "new arrow"
+            if (in[0] > minEnd) {
+                count++;
+                minEnd = in[1];
+            } else {
+                // renew key parameters of the active set
+                minEnd = Math.min(minEnd, in[1]);
+            }
+        }
+        return count + 1;
+    }
+}
