ad30c9555d041a21599b4d37dd2760c08bf60a88
Shruti Singhania <shrutisinghania75@gmail.com>
2022-01-17 23:30:12 +0530

    Time: 1 ms (86.67%), Space: 36.9 MB (80.77%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr= s.split(" ");
+        if(arr.length!= pattern.length())
+            return false;
+        HashMap<Character, String> map = new HashMap<Character, String>();
+        for(int i=0; i<arr.length; i++){
+            char c = pattern.charAt(i);
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(arr[i]))
+                    return false;
+            }else{
+                if(map.containsValue(arr[i]))
+                    return false;
+                map.put(c, arr[i]);
+            }    
+        }
+        return true;
+    }
+}
