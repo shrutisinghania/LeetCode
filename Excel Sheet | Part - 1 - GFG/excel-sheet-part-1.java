a58b3d08c3f006463ac7b707504560ef51808596
Shruti Singhania <shrutisinghania75@gmail.com>
2022-02-03 17:46:08 +0530

    Added solution - LeetHub

---
+++ Excel Sheet | Part - 1 - GFG/excel-sheet-part-1.java
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	    int t=Integer.parseInt(br.readLine());
+    
+	    while(t > 0){
+	        int n = Integer.parseInt(br.readLine());
+	        Solution T = new Solution();
+	        
+	        System.out.println(T.excelColumn(n));
+	        
+            t--;
+	    }
+	}
+    
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public String excelColumn(int N){
+       String res="";
+        while(N>0){
+            int rem=N%26;
+            res=(rem==0)?"Z"+res:((char)((rem-1)+'A'))+res;
+            N=(rem==0)?N/26-1:N/26;
+        }
+        return res;
+    }
+}
