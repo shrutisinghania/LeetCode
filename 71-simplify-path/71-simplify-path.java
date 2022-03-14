e78b14c7044daf695f1b6246d2efc0e2f15a352e
Shruti Singhania <shrutisinghania75@gmail.com>
2022-03-14 21:05:38 +0530

    Time: 2 ms (99.95%), Space: 42.4 MB (66.42%) - LeetHub

--- 71-simplify-path/71-simplify-path.java
+++ 71-simplify-path/71-simplify-path.java
@@ -1,16 +1,27 @@
 class Solution {
     public String simplifyPath(String path) {
-        Deque<String> stack = new LinkedList<>();
-        Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
-        for (String dir : path.split("/")) {
-            if (dir.equals("..") && !stack.isEmpty()) 
-                stack.pop();
-            else if (!skip.contains(dir)) 
-                stack.push(dir);
+       Deque<String> dq = new LinkedList<>();
+        int i = 0;
+        while (i < path.length()) {
+            ++i;
+            int l = i;
+            while (i < path.length() && path.charAt(i) != '/') ++i;
+            String segment = path.substring(l, i);
+            
+            if (segment.equals(".") || segment.isEmpty()) continue;
+            if (segment.equals("..")) {
+                if (!dq.isEmpty()) dq.pollLast();
+            } else {
+                dq.offerLast(segment);
+            }
         }
-        String res = "";
-        for (String dir : stack) 
-            res = "/" + dir + res;
-        return res.isEmpty() ? "/" : res;
+        
+        if (dq.isEmpty()) return "/";
+        
+        StringBuilder result = new StringBuilder();
+        while (!dq.isEmpty()) {
+            result.append("/").append(dq.pollFirst());
+        }
+        return result.toString();
     }
 }
