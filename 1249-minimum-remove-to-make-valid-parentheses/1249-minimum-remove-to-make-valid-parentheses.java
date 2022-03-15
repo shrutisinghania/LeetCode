b159332d7e4070867e7f331167e4436c5712ec69
Shruti Singhania <shrutisinghania75@gmail.com>
2022-03-15 19:54:22 +0530

    Time: 8 ms (99.45%), Space: 49.7 MB (67.69%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,19 +1,23 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-      StringBuilder sb = new StringBuilder(s), sb1 = new StringBuilder();
-      Stack<Integer> st = new Stack();
-      for (int i = 0; i < sb.length(); ++i) {
-        if (sb.charAt(i) == '(') st.add(i + 1);
-        if (sb.charAt(i) == ')') {
-          if (!st.empty() && st.peek() >= 0) st.pop();
-          else st.add(-(i + 1));
+        char[] arr = s.toCharArray();
+        int open = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '(') open++;
+            else if (arr[i] == ')') {
+                if (open == 0) arr[i] = '*';
+                else open--;
+            }
         }
-      }
-      for(int i = 0, j = 0; i < sb.length(); ++i) {
-          if (j >= st.size() || i != Math.abs(st.elementAt(j)) - 1) {
-            sb1.append(sb.charAt(i));
-          } else ++j;
-      }
-      return sb1.toString(); 
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (open > 0 && arr[i] == '(') {
+                arr[i] = '*';
+                open--;
+            }
+        }
+        int p = 0;
+        for (int i = 0; i < arr.length; i++) 
+            if (arr[i] != '*') arr[p++] = arr[i];
+        return new String(arr).substring(0, p);
     }
 }
