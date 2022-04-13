dc48da72b6d80dcc6956fc6ba91e6ea6053e0beb
Shruti Singhania <shrutisinghania75@gmail.com>
2022-04-13 11:44:22 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (90.73%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int cnt = 1, d = 0, row = 0, col = 0;
+        int dir[][] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        while (cnt <= n * n) {
+            result[row][col] = cnt++;
+            int r = Math.floorMod(row + dir[d][0], n);
+            int c = Math.floorMod(col + dir[d][1], n);
+            if (result[r][c] != 0) 
+                d = (d + 1) % 4;
+            row += dir[d][0];
+            col += dir[d][1];
+        }
+        return result;
+    }
+}
