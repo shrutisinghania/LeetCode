dde9265225bc80e5c75526cd8aabbbeaf967c397
Shruti Singhania <shrutisinghania75@gmail.com>
2022-04-19 17:51:52 +0530

    Time: 4 ms (38.53%), Space: 46.9 MB (67.61%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void recoverTree(TreeNode root) {
+        TreeNode pre = null;
+        TreeNode first = null, second = null;
+        TreeNode temp = null;
+		while(root!=null){
+			if(root.left!=null){
+				// connect threading for root
+				temp = root.left;
+				while(temp.right!=null && temp.right != root)
+					temp = temp.right;
+				// the threading already exists
+				if(temp.right!=null){
+				    if(pre!=null && pre.val > root.val){
+				        if(first==null)
+                            first = pre;
+				        second = root;
+				    }
+				    pre = root;
+					temp.right = null;
+					root = root.right;
+				}else{
+					// construct the threading
+					temp.right = root;
+					root = root.left;
+				}
+			}else{
+				if(pre!=null && pre.val > root.val){
+				    if(first==null)
+                        first = pre;
+				    second = root;
+				}
+				pre = root;
+				root = root.right;
+			}
+		}
+		// swap two node values;
+		if(first!= null && second != null){
+		    int t = first.val;
+		    first.val = second.val;
+		    second.val = t;
+		}
+    }
+}
