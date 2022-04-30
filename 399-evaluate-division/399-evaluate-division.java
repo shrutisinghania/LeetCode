d2a531b0ccefdc3e2e499629bd5944b2457338c0
Shruti Singhania <shrutisinghania75@gmail.com>
2022-04-30 14:43:05 +0530

    Time: 0 ms (100.00%), Space: 41 MB (83.56%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.java
+++ 399-evaluate-division/399-evaluate-division.java
@@ -1,44 +1,44 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        // build big double direction map
-        HashMap<String, HashMap<String, Double>> map = new HashMap<>();
+        Map<String, String> rootMap = new HashMap<>();  // {val, root}
+        Map<String, Double> distMap = new HashMap<>();
         for (int i = 0; i < equations.size(); i++) {
-            String start = equations.get(i).get(0);
-            String end = equations.get(i).get(1);
-            double ratio = values[i];
-            map.putIfAbsent(start, new HashMap<>());
-            map.get(start).put(end, ratio);
-            map.putIfAbsent(end, new HashMap<>());
-            map.get(end).put(start, 1.0 / ratio);
+            String Ai = equations.get(i).get(0), Bi = equations.get(i).get(1);
+            String r1 = find(rootMap, distMap, Ai);
+            String r2 = find(rootMap, distMap, Bi);
+            rootMap.put(r1, r2);
+            distMap.put(r1, distMap.get(Bi) * values[i] / distMap.get(Ai));
         }
-        // deal with every query
         double[] res = new double[queries.size()];
         for (int i = 0; i < queries.size(); i++) {
-            String start = queries.get(i).get(0);
-            String end = queries.get(i).get(1);
-            // first check if start or end exist in big map or not
-            if (!map.containsKey(start) || !map.containsKey(end)) {
+            String Cj = queries.get(i).get(0), Dj = queries.get(i).get(1);
+            if (!rootMap.containsKey(Cj) || !rootMap.containsKey(Dj)) {
                 res[i] = -1.0;
                 continue;
             }
-            // enter dfs loop, for each query, there is a new visited set
-            res[i] = helper(map, start, end, new HashSet<>());
+            String r1 = find(rootMap, distMap, Cj);
+            String r2 = find(rootMap, distMap, Dj);
+            if (!r1.equals(r2)) {
+                res[i] = -1.0;
+                continue;
+            }
+            res[i] = (double) distMap.get(Cj) / distMap.get(Dj);
         }
         return res;
     }
-    public double helper(HashMap<String, HashMap<String, Double>> map, String start, String end, HashSet<String> visited) {
-        // actually no need to check quitting condition, because if one can enter this dfs, one must iterate all children
-        // if (!map.containsKey(start)) return -1;
-        if (map.get(start).containsKey(end))
-            return map.get(start).get(end);
-        // mark visited
-        visited.add(start);
-        for (Map.Entry<String, Double> entry : map.get(start).entrySet()) {
-            if (visited.contains(entry.getKey())) continue;
-            double res = helper(map, entry.getKey(), end, visited);
-            if (res == -1.0) continue;
-            return res * entry.getValue();
+    
+    String find(Map<String, String> rootMap, Map<String, Double> distMap, String s) {
+        if (!rootMap.containsKey(s)) {
+            rootMap.put(s, s);
+            distMap.put(s, 1.0);
+            return s;
         }
-        return -1.0;
+        if (rootMap.get(s).equals(s)) return s;
+        // find further root, update it
+        String lastP = rootMap.get(s);
+        String p = find(rootMap, distMap, lastP);
+        rootMap.put(s, p);
+        distMap.put(s, distMap.get(s) * distMap.get(lastP));
+        return p;
     }
 }
