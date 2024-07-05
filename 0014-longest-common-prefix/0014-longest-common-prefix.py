ab1a8b880f04d934e6fe7bc7814c9600cbca7694
Shruti Singhania <shrutisinghania75@gmail.com>
2024-07-05 10:40:38 +0530

    Time: 38 ms (56.51%), Space: 16.4 MB (94.66%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,42 +1,10 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        class Node:
-            def __init__(self):
-                self.childnode = [None] * 26
-                self.end = False
-                
-                
-        class Trie():
-            def __init__(self):
-                self.root = Node()
-            
-            def insert(self, s):
-                cn = self.root
-                for c in s.lower():
-                    n = ord(c) - ord('a')
-                    if not cn.childnode[n]:
-                        cn.childnode[n] = Node()
-                    cn = cn.childnode[n]
-                cn.end = True
-                    
-            def lcs(self, s):
-                cn = self.root
-                sub = ""
-                for c in s.lower():
-                    n = ord(c) - ord('a')
-                    if not cn.childnode[n] or cn.end:
-                        return sub
-                    cn = cn.childnode[n]
-                    sub += c
-                return sub
-        
-        t = Trie()
-        t.insert(strs[0])
-        s = strs[0]
+        pre = strs[0]
         for i in strs[1:]:
-            s = t.lcs(i)
-            if not s or len(s) == 0:
-                return ""
-            t.insert(s)
-        return s
+            while i[:len(pre)] != pre: 
+                pre = pre[:-1]
+                if not pre:
+                    return ""
+        return pre
             
