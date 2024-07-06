018ef62c0d7a810ca3d886eb9b9f180f6b37e487
Shruti Singhania <shrutisinghania75@gmail.com>
2024-07-06 23:29:07 +0530

    Time: 661 ms (71.00%), Space: 20.8 MB (37.41%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,36 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        def kSum(nums, target, k):
+            res = []
+            if not nums:
+                return res
+            n = target // k
+            if n < nums[0] or n > nums[-1]:
+                return res
+            if k == 2:
+                return twoSum(nums, target)
+            for i in range(len(nums)):
+                if i == 0 or nums[i] != nums[i-1]:
+                    for j in kSum(nums[i+1:], target-nums[i], k-1):
+                        res.append([nums[i]] + j)
+            return res
+            
+            
+        def twoSum (nums, target):
+            res = []
+            l = 0
+            r = len(nums) - 1
+            while l<r:
+                if nums[l] + nums[r] > target or (r < len(nums)-1 and nums[r] == nums[r+1]):
+                    r -= 1
+                elif nums[l] + nums[r] < target or (l > 0 and nums[l] == nums[l-1]):
+                    l += 1
+                else:
+                    res.append([nums[l],nums[r]])
+                    l += 1
+                    r -= 1
+            return res
+            
+        nums.sort()
+        return kSum(nums, 0, 3)
+        
