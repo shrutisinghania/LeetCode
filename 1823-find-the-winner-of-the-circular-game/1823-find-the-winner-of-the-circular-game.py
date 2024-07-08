6f3798bbc1619a1a126c62256adef524f2bb7e51
Shruti Singhania <shrutisinghania75@gmail.com>
2024-07-08 18:04:42 +0530

    Time: 47 ms (36.44%), Space: 16.7 MB (8.19%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -7,3 +7,10 @@ class Solution:
             prev = (prev + k) % len(l)
             l.pop(prev)
         return l[0]
+#     def findTheWinner(self, n: int, k: int) -> int:
+#         return self.winnerHelper(n, k) + 1
+
+#     def winnerHelper(self, n: int, k: int) -> int:
+#         if n == 1:
+#             return 0
+#         return (self.winnerHelper(n - 1, k) + k) % n
