a60bb32019436049de3c2be4cdd7fa903e5689de
Shruti Singhania <shrutisinghania75@gmail.com>
2024-08-24 22:41:16 +0530

    Time: 34 ms (76.68%), Space: 16.8 MB (20.99%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -7,20 +7,19 @@
 from collections import defaultdict
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        def findDepth(self, root, c, r):
+        d = defaultdict(list)
+        def findDepth(root, c, r):
             if not root:
                 return
-            self.d[c].append((r, root.val))
-            findDepth(self, root.left, c - 1, r+1)
-            findDepth(self, root.right, c + 1, r+1)            
-        
-        self.d = defaultdict(list)
-        findDepth(self, root, 0, 0)
+            d[c].append((r, root.val))
+            findDepth(root.left, c-1, r+1)
+            findDepth(root.right, c+1, r+1)
+           
+        findDepth(root, 0, 0)
         res = []
-        for i in sorted(self.d):
+        for i in sorted(d):
             l = []
-            for a, b in sorted(self.d[i]):
+            for a, b in sorted(d[i]):
                 l.append(b)
             res.append(l)
         return res
-        
