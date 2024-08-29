e53ef85ada2a4599df6adaf60b13d220cea0a178
Shruti Singhania <shrutisinghania75@gmail.com>
2024-08-29 20:04:46 +0530

    Time: 35 ms (78.5%), Space: 16.7 MB (9.54%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix) - 1
+
+        def rotateDigit(xr, xc):
+            val = matrix[xr][xc]
+            for i in range(4):
+                r = xc
+                c = n - xr
+                temp = matrix[r][c]
+                matrix[r][c] = val
+                val = temp
+                xr = r
+                xc = c
+
+
+        for i in range((n//2)+1):
+            for j in range(i, n-i):
+                print(i, j)
+                rotateDigit(i, j)
