92848b63e54567fd679ce4507511e0282da329f5
Shruti Singhania <shrutisinghania75@gmail.com>
2024-08-30 13:48:26 +0530

    Time: 41 ms (17.05%), Space: 16.4 MB (96.99%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,11 +1,14 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def rec(p, n):
-            if n == 0 or p == 1:
-                return 1
-            r = rec(p * p, n//2)
-            return r * p if n % 2 != 0 else r
-
-        if n <= -1:
-            return 1/rec(x, -n)
-        return rec(x, n)
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n == -1:
+            return 1/x
+        a = self.myPow(x, n//2) 
+        if n % 2 == 0:
+            return a * a
+        else:
+            return a * a * x
+        
