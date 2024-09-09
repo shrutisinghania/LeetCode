7dc43769c6f98062e1c5821d25a1b4077890b584
Shruti Singhania <shrutisinghania75@gmail.com>
2024-09-09 13:05:50 +0530

    Time: 51 ms (76.38%), Space: 16.8 MB (28.28%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,26 +5,32 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        c = 0
-        l = ListNode()
-        res = l
-        while l1 or l2 or c:
-            val = 0
-            if l1:
-                val += l1.val
-                l1 = l1.next
-            if l2:
-                val += l2.val
-                l2 = l2.next
-                
-            val += c
-            t = ListNode(val % 10)
+        def addList(l, l1, l2, c):
+            if not l1 and not l2:
+                if c:
+                    l.next = ListNode(c)
+                return
+            elif not l1:
+                val = l2.val + c
+                l2.val = val % 10
+                c = val // 10
+                l.next = l2
+                return addList(l.next, l1, l2.next, c)
+            elif not l2:
+                val = l1.val + c
+                l1.val = val % 10
+                c = val // 10
+                l.next = l1
+                return addList(l.next, l1.next, l2, c)
+            
+            val = l1.val + l2.val + c
+            l1.val = val % 10
             c = val // 10
-            
-            l.next = t
-            l = l.next
-        return res.next
-            
-            
-            
+            l.next = l1
+            return addList(l.next, l1.next, l2.next, c)
+        
+        l = ListNode()
+        addList(l, l1, l2, 0)
+        return l.next
+
             
