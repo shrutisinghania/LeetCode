6e171d88a5b3849f1b71ed5df0c5bad14ac6e7e7
Shruti Singhania <shrutisinghania75@gmail.com>
2024-09-14 23:33:03 +0530

    Time: 30 ms (83.52%), Space: 16.4 MB (90.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        ways = 1
-
-        for i in range(1, (n // 2) + 1):
-            product = 1
-
-            for j in range(i, 2 * i):
-                product *= (n - j) / (j - i + 1)
-
-            ways += product
-
-        return int(ways)
+        if n == 1:
+            return 1
+        a = [0 for _ in range(n+1)]
+        a[0] = 1
+        a[1] = 1
+        for i in range(2, n+1):
+            a[i] = a[i-1] + a[i-2]
+        return a[n]
