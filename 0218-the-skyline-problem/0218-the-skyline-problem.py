7dea1e981ee2ed76c841ecdb59fc641fa0b66262
Shruti Singhania <shrutisinghania75@gmail.com>
2024-09-17 02:18:39 +0530

    Time: 103 ms (56.56%), Space: 23.1 MB (11.13%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.py
+++ 0218-the-skyline-problem/0218-the-skyline-problem.py
@@ -10,14 +10,19 @@ class Solution:
         heapq.heappush(q, 0)
         prev_max_height = -1
         res = []
+        height_dict = {}
+        height_dict[0] = 1
         for building in building_list:
-            if building[1] < 0:
-                q.remove(building[1])
-                heapq.heapify(q)
-            else:
+            if building[1] > 0:
                 heapq.heappush(q , -building[1])
+                height_dict[building[1]] = height_dict.get(building[1], 0) + 1
+            else:
+                height_dict[-building[1]] -= 1
             
             max_height = -q[0] if q[0] < 0 else 0
+            while q and height_dict[max_height] == 0:
+                heapq.heappop(q)
+                max_height = -q[0] if q[0] < 0 else 0
 
             if prev_max_height != max_height:
                 res.append((building[0], max_height))
