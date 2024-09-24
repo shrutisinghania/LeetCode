70bf98045b548ce514d618b1947a28630f0b4f50
Shruti Singhania <shrutisinghania75@gmail.com>
2024-09-24 16:41:13 +0530

    Time: 33 ms (70.73%), Space: 16.6 MB (50.63%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,21 +1,23 @@
 class Solution:
     def wordBreak(self, st: str, wordDict: List[str]) -> List[str]:
-        def rec(l, s):
-            if len(s) == 0:
-                res.append(l.lstrip())
-                return
-            # if s in d:
-            #     return d[s]
+        def rec(s):
+            if s in d:
+                return d[s]
+            
+            if not s:
+                return [""]
+            res = []
             for i in words:
                 if s.startswith(i):
-                    new_s = l + " " + i 
-                    rec(new_s, s[len(i):])
-                        # d[s] = True
+                    a = rec(s[len(i):])
+                    for f in a:
+                        p = i + " " + f 
+                        res.append(p.rstrip())
+            d[s] = res
+            return res
 
 
         words = set(wordDict)
-        d = {}
-        res = []
-        rec('', st)
-        return res
+        d = {}      
+        return rec(st)
 
